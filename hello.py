603378ca925e471c4ff2803e86148b7d7ceb07ac
Nakampom Team <ookza5949@gmail.com>
2026-01-07 10:29:39 +0000

    add new function print hello

--- hello.py
+++ hello.py
@@ -1,3 +1,10 @@
 ## this is my first scirpt
 print("hello world")
 print("I am using github with posit.cloud")
+
+def hello():
+  name = input("what is your name: ")
+  print(f"Hello {name}!")
+  
+
+hello()
